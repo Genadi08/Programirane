6fc648dbb70654a394c4cf7511168229c6e226fe
HristoHacker123 <hrisi.belezirev@gmail.com>
2025-10-28 14:27:27 +0200

    purvi put

---
+++ PrimeNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prostiChisla
+{
+    public class PrimeNumbers
+    {
+        public List<int> FindPrimeNumbers(List<int> inputNumbers)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < inputNumbers.Count; i++)
+            {
+                if (IsPrime(inputNumbers[i]) == true)
+                {
+                    result.Add(inputNumbers[i]);
+
+
+                }
+           
+            }
+
+
+            return result;
+
+        }
+        public bool IsPrime(int number)
+        {
+            if (number < 0) number = -number;
+            bool flag= false;
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return flag;
+                }
+            }
+            return true; //the number is prime
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace prostiChisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vliza spisuk ot shisla i programata vrucha spisuk
+            //programata poema ili vkarva spisuk ot int-ove {list}  
+            Console.WriteLine("Input number");
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            PrimeNumbers sito = new PrimeNumbers();
+            List<int> resPrimeNumber = sito.FindPrimeNumbers(list);
+            foreach (int item in resPrimeNumber)
+            {
+                Console.WriteLine(item + " ,  ");
+            }
+            {
+                Console.WriteLine(string.Join(", ", list));
+            }
+        }
+    }
+}
